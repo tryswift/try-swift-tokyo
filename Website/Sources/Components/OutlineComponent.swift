b4b6b6b41661dd367a1ae7ce49e5c7fd06340001
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2025-09-24 01:02:44 +0900

    ♻️ Refactor code

--- Website/Sources/Components/OutlineComponent.swift
+++ Website/Sources/Components/OutlineComponent.swift
@@ -5,49 +5,52 @@ struct OutlineComponent: HTML {
   let language: SupportedLanguage
 
   var body: some HTML {
+    let data: OutlineData = switch year {
+    case .year2025: .year2025
+    case .year2026: .year2026
+    }
+
     Table {
       Row {
         Column {
-          switch year {
-          case .year2025:
-            String("Date and time", language: language)
-          case .year2026:
-            String("Workshop", language: language)
-          }
+          String(data.firstLabel, language: language)
         }
         .fontWeight(.bold)
         .foregroundStyle(.dimGray)
         Column {
-          switch year {
-          case .year2025:
-            String("Apr. 9th - 11th, 2025", language: language)
-          case .year2026:
-            String("Apr. 12th, 2026 (JST)<br><br>Venue:<br>Tokyo, Japan", language: language)
-          }
+          String(data.firstValue, language: language)
         }
         .foregroundStyle(.dimGray)
       }
       Row {
         Column {
-          switch year {
-          case .year2025:
-            String("Venue", language: language)
-          case .year2026:
-            String("Conference", language: language)
-          }
+          String(data.secondLabel, language: language)
         }
         .fontWeight(.bold)
         .foregroundStyle(.dimGray)
         Column {
-          switch year {
-          case .year2025:
-            String("TACHIKAWA STAGE GARDEN<br>N1, 3-3, Midori-cho, Tachikawa, Tokyo 190-0014", language: language)
-          case .year2026:
-            String("Apr. 13th – 14th, 2026 (JST)<br><br>Venue:<br>TACHIKAWA STAGE GARDEN<br>N1, 3-3, Midori-cho, Tachikawa, Tokyo 190-0014", language: language)
-          }
+          String(data.secondValue, language: language)
         }
         .foregroundStyle(.dimGray)
       }
     }
   }
+}
+
+private struct OutlineData {
+  let firstLabel, firstValue, secondLabel, secondValue: String
+
+  static let year2025 = OutlineData(
+    firstLabel: "Date and time",
+    firstValue: "Apr. 9th - 11th, 2025",
+    secondLabel: "Venue",
+    secondValue: "TACHIKAWA STAGE GARDEN<br>N1, 3-3, Midori-cho, Tachikawa, Tokyo 190-0014"
+  )
+
+  static let year2026 = OutlineData(
+    firstLabel: "Workshop",
+    firstValue: "Apr. 12th, 2026 (JST)<br><br>Venue:<br>Tokyo, Japan",
+    secondLabel: "Conference",
+    secondValue: "Apr. 13th – 14th, 2026 (JST)<br><br>Venue:<br>TACHIKAWA STAGE GARDEN<br>N1, 3-3, Midori-cho, Tachikawa, Tokyo 190-0014"
+  )
 }
