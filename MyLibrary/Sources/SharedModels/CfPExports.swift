183b98380eeb69f5b372dd40f08ca137a2402c51
Daiki Matsudate <20222809+d-date@users.noreply.github.com>
2026-01-22 14:32:40 +0800

    Fix

--- MyLibrary/Sources/SharedModels/CfPExports.swift
+++
@@ -1,8 +0,0 @@
-// Re-export all CfP types from the SharedModels module
-// This file ensures all types in the CfP subfolder are accessible
-
-@_exported import struct Foundation.UUID
-@_exported import struct Foundation.Date
-
-// CfP types are automatically included as they are in the same module
-// This file exists to ensure the CfP folder is properly compiled as part of SharedModels
