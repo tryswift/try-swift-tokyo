08071c4068181536ed5d540501a953ca0306e5bb
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2026-01-27 11:24:20 +0900

    🐛 Fix layout

--- Website/Sources/Components/WorkshopComponent.swift
+++ Website/Sources/Components/WorkshopComponent.swift
@@ -8,39 +8,20 @@ struct WorkshopComponent: HTML {
 
   var body: some HTML {
     ForEach(workshops) { workshop in
-      HStack {
-        HStack(spacing: 0) {
-          let speakerImageSize = workshop.speakers!.count > 1 ? 115 : 230
-          ForEach(workshop.speakers!) { speaker in
-            Image(speaker.imageFilename, description: speaker.name)
-              .resizable()
-              .frame(maxWidth: speakerImageSize, maxHeight: speakerImageSize)
-              .cornerRadius(speakerImageSize / 2)
-              .onClick {
-                ShowModal(id: speaker.modalId)
-              }
+      Section {
+        Section {
+          HStack {
+            contents(workshop: workshop)
           }
         }
+        .hidden(.responsive(true, medium: true, large: false, xLarge: false, xxLarge: false))
+
         Section {
-          Text(workshop.title)
-            .font(.title4)
-            .fontWeight(.medium)
-            .foregroundStyle(.orangeRed)
-
-          Text(workshop.speakers!.map(\.name).joined(separator: ", "))
-            .font(.lead)
-            .fontWeight(.thin)
-            .foregroundStyle(.dimGray)
-
-          let localizedDescription = String(workshop.description!, bundle: .scheduleFeature, language: language)
-            .convertNewlines()
-          Text(markdown: localizedDescription)
-            .font(.lead)
-            .fontWeight(.thin)
-            .foregroundStyle(.dimGray)
-            .padding(.leading, .px(16))
+          VStack(spacing: .small) {
+            contents(workshop: workshop)
+          }
         }
-        .horizontalAlignment(.leading)
+        .hidden(.responsive(false, medium: false, large: true, xLarge: true, xxLarge: true))
       }
       .padding(.bottom, .px(32))
     }
@@ -51,5 +32,41 @@ struct WorkshopComponent: HTML {
         SpeakerModal(year: year, speaker: speaker, language: language)
       }
     }
+  }
+
+  @HTMLBuilder
+  private func contents(workshop: Session) -> some HTML {
+    HStack(spacing: 0) {
+      let speakerImageSize = workshop.speakers!.count > 1 ? 115 : 230
+      ForEach(workshop.speakers!) { speaker in
+        Image(speaker.imageFilename, description: speaker.name)
+          .resizable()
+          .frame(maxWidth: speakerImageSize, maxHeight: speakerImageSize)
+          .cornerRadius(speakerImageSize / 2)
+          .onClick {
+            ShowModal(id: speaker.modalId)
+          }
+      }
+    }
+    Section {
+      Text(workshop.title)
+        .font(.title4)
+        .fontWeight(.medium)
+        .foregroundStyle(.orangeRed)
+
+      Text(workshop.speakers!.map(\.name).joined(separator: ", "))
+        .font(.lead)
+        .fontWeight(.thin)
+        .foregroundStyle(.dimGray)
+
+      let localizedDescription = String(workshop.description!, bundle: .scheduleFeature, language: language)
+        .convertNewlines()
+      Text(markdown: localizedDescription)
+        .font(.lead)
+        .fontWeight(.thin)
+        .foregroundStyle(.dimGray)
+        .padding(.leading, .px(16))
+    }
+    .horizontalAlignment(.leading)
   }
 }
