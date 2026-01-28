0e901fd7c85fde901cc3e1c17b437dd802806e29
Daiki Matsudate <20222809+d-date@users.noreply.github.com>
2026-01-28 19:24:12 +0900

    format

--- LocalizationGenerated/Package.swift
+++ LocalizationGenerated/Package.swift
@@ -2,33 +2,33 @@
 import PackageDescription
 
 let package = Package(
-    name: "LocalizationGenerated",
-    platforms: [
-        .macOS(.v14),
-        .iOS(.v17)
-    ],
-    products: [
-        .library(
-            name: "LocalizationGenerated",
-            targets: ["LocalizationGenerated"]
-        )
-    ],
-    dependencies: [
-        .package(path: "../SharedModels")
-    ],
-    targets: [
-        .target(
-            name: "LocalizationGenerated",
-            dependencies: [
-                .product(name: "SharedModels", package: "SharedModels")
-            ],
-            plugins: [
-                "LocalizationCodegenPlugin"
-            ]
-        ),
-        .plugin(
-            name: "LocalizationCodegenPlugin",
-            capability: .buildTool()
-        )
-    ]
+  name: "LocalizationGenerated",
+  platforms: [
+    .macOS(.v14),
+    .iOS(.v17),
+  ],
+  products: [
+    .library(
+      name: "LocalizationGenerated",
+      targets: ["LocalizationGenerated"]
+    )
+  ],
+  dependencies: [
+    .package(path: "../SharedModels")
+  ],
+  targets: [
+    .target(
+      name: "LocalizationGenerated",
+      dependencies: [
+        .product(name: "SharedModels", package: "SharedModels")
+      ],
+      plugins: [
+        "LocalizationCodegenPlugin"
+      ]
+    ),
+    .plugin(
+      name: "LocalizationCodegenPlugin",
+      capability: .buildTool()
+    ),
+  ]
 )
