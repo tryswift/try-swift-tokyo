a543218eaf33f304f31241b0ce8ab45ce81849c5
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2026-03-01 01:04:10 +0900

    ✨ Added events section

---
+++ Website/Sources/Components/EventsComponent.swift
@@ -0,0 +1,58 @@
+import Ignite
+import SharedModels
+
+struct EventsComponent: HTML {
+  struct Event {
+    let name: String
+    let imageName: String
+    let url: String
+
+    var imageFilename: String {
+      "/images/events/\(imageName).png"
+    }
+  }
+
+  let year: ConferenceYear
+  let events: [Event]
+
+  init(year: ConferenceYear) {
+    self.year = year
+
+    events = switch year {
+    case .year2026: [
+      Event(
+        name: "try! Swift Tokyo 2026 ハッカソン for Students",
+        imageName: "event-2026-1",
+        url: "https://connpass.com/event/383016/"
+      ),
+      Event(
+        name: "Sakura.swift #1",
+        imageName: "event-2026-2",
+        url: "https://sakuraswift.connpass.com/event/385856/"
+      ),
+    ]
+    default: []
+    }
+  }
+
+  var body: some HTML {
+    CenterAlignedGrid(events, columns: 2) { event in
+      VStack(alignment: .center) {
+        let image = Image(event.imageFilename, description: event.name)
+          .resizable()
+          .frame(maxWidth: 307, maxHeight: 230)
+          .cornerRadius(20)
+          .margin(.bottom, .px(16))
+
+        Link(image, target: event.url)
+          .target(.newWindow)
+        Link(event.name, target: event.url)
+          .target(.newWindow)
+          .font(.title4)
+          .fontWeight(.medium)
+          .foregroundStyle(.orangeRed)
+      }
+      .margin(.bottom, .px(32))
+    }
+  }
+}
