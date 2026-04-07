bf897e58c292c55918d5e9e8ca061af8aae0de1e
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2026-04-08 04:46:45 +0900

    :lipstick: update booth map image and page layout

--- Website/Sources/Pages/BoothMap.swift
+++ Website/Sources/Pages/BoothMap.swift
@@ -5,8 +5,19 @@ struct BoothMap: StaticPage {
   var path = "/booth-map"
 
   var body: some HTML {
-    Image("/images/booth-map.png", description: "Booth Map")
-      .resizable()
-      .frame(maxWidth: .percent(100%))
+    Section {
+      Image("/images/booth-map.png", description: "Booth Map")
+        .resizable()
+        .frame(width: .percent(100%), height: .percent(100%))
+        .style(.objectFit, "contain")
+    }
+    .ignorePageGutters()
+    .frame(height: .vh(100%))
+    .style(.display, "flex")
+    .style(.alignItems, "center")
+    .style(.justifyContent, "center")
+    .background(
+      Gradient(colors: [.init(hex: "#BDA4C4"), .init(hex: "#B29AC2"), .init(hex: "#9881BF")], type: .linear(angle: 180))
+    )
   }
 }
