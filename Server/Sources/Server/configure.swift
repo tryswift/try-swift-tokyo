9527bbad379ec11adbaa210ff78cabe100b04c91
sya-ri <git@s7a.dev>
2026-04-18 14:44:57 +0900

    Remove workshop seed migration

--- Server/Sources/Server/configure.swift
+++ Server/Sources/Server/configure.swift
@@ -62,7 +62,6 @@ enum AppConfiguration {
     app.migrations.add(CreateFavorite())
     app.migrations.add(AddFavoriteIndexes())
     app.migrations.add(AddFeedbackIndexes())
-    app.migrations.add(SeedTrySwiftTokyo2026Workshops())
 
     // Auto-migrate on startup (safe for production as Fluent tracks completed migrations)
     try await app.autoMigrate()
